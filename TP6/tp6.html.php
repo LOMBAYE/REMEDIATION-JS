7d14b290e48cc0b1364078b3e1855f77070bb670
LOMBAYE <lombayedev@gmail.com>
2022-03-17 11:16:59 +0000

    first commit

---
+++ TP6/tp6.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tp6.css">
+    <title>TP6</title>
+</head>
+<body>
+    <div class="container"></div>
+    <script src="tp6.js"></script>
+</body>
+</html>

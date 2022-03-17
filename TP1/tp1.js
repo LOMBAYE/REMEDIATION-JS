519e105c1de9f6b94820e28d44aba48309d30f10
LOMBAYE <lombayedev@gmail.com>
2022-03-17 19:19:10 +0000

    stylise le tp2

--- TP1/tp1.js
+++ TP1/tp1.js
@@ -1,19 +1,19 @@
 const units = document.querySelectorAll('.unit')
 h3 = document.querySelector('h3')
-
-function listening() {
-    units.forEach(unit => listener(unit))
-}
-listening()
+units.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+        const P = listener(e)
+        h3.appendChild(P);
+        setTimeout(() => h3.removeChild(P), 3000);
+    })
+});
 
 function listener(e) {
-    e.addEventListener('click', function() {
-
-        var bodyCSS = window.getComputedStyle(e, null);
-        var bgColor = bodyCSS.getPropertyValue('background-color');
-        h3.style.backgroundColor = bgColor
-        h3.innerText = e.innerText
-        h3.style.visibility = 'visible';
-
-    })
+    P = document.createElement('p');
+    P.classList.add('general')
+    var eCSS = window.getComputedStyle(e.target, null);
+    var bgColor = eCSS.getPropertyValue('background-color');
+    P.style.backgroundColor = bgColor
+    P.innerText = e.target.innerText
+    return P
 }

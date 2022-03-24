efdbcae4f298265845ea5f92d971598206d247d5
LOMBAYE <lombayedev@gmail.com>
2022-03-24 12:39:56 +0000

    tp6:random recipe

--- TP6/tp6.js
+++ TP6/tp6.js
@@ -1,14 +1,43 @@
-const getMeals = async(page, limit) => {
-    const API_URL = "https://www.themealdb.com/api/json/v1/1/random.php"
-    const response = await fetch(API_URL);
-    return await response.json();
-}
-console.log(getMeals())
-const search_id = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52913"
-const search_name = "https://www.themealdb.com/api/json/v1/1/search.php?s=Brie"
+const form = document.querySelector('form'),
+    generate_recipe = document.querySelector('.generate_recipe'),
+    meal_name = document.querySelector('.meal_name'),
+    display_recipe = document.querySelector('.display_recipe'),
+    search_input = document.querySelector('.search_input'),
+    img2 = document.querySelector('#img2')
 
-// fetch(API_URL)
-//     .then(response => { return (response.json()) })
-//     .then(data => {
-//         console.log(data)
-//     })
+
+// img2.addEventListener('click', alert('okii'))
+
+
+const random_search = "https://www.themealdb.com/api/json/v1/1/random.php",
+    id_search = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52913",
+    name_search = "https://www.themealdb.com/api/json/v1/1/search.php?s=Brie"
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault
+    alert('okkkkkk')
+})
+
+function searchById(idMeal) {
+    fetch(id_search + idMeal)
+        .then(res => { console.log(res.json()) })
+}
+
+generate_recipe.addEventListener('click', () => {
+    fetch(random_search)
+        .then(response => { return (response.json()) })
+        .then(data => {
+            data.meals.forEach(meal => {
+                // console.log(meal)
+                let image = meal.strMealThumb
+                img2.src = image
+                meal_name.innerHTML = meal.strMeal
+            })
+        })
+})
+
+display_recipe.addEventListener('click', (e) => {
+    e.preventDefault
+    alert('recette')
+})

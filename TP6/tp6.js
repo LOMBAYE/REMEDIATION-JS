505b3d954639d181c13e1ed2645189c9a956347a
LOMBAYE <lombayedev@gmail.com>
2022-03-24 23:56:10 +0000

    tp6:search recipe by id and by name

--- TP6/tp6.js
+++ TP6/tp6.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form'),
     generate_recipe = document.querySelector('.generate_recipe'),
     meal_name = document.querySelector('.meal_name'),
     display_recipe = document.querySelector('.display_recipe'),
-    search_input = document.querySelector('.search_input'),
+    input = document.querySelector('input'),
     img2 = document.querySelector('#img2'),
     img_recette = document.querySelector('.img_recette'),
     recette = document.querySelector('.recette'),
@@ -12,24 +12,44 @@ const form = document.querySelector('form'),
     ingredients = document.querySelector('ul')
 
 
-// ingredients.addEventListener('click', () => alert('okii'))
+// input.addEventListener('click', () => alert('okii'))
 
 
 const random_search = "https://www.themealdb.com/api/json/v1/1/random.php",
-    id_search = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52913",
-    name_search = "https://www.themealdb.com/api/json/v1/1/search.php?s=Brie"
+    id_search = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=",
+    name_search = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault
-    alert('okkkkkk')
-})
+
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault
+//     const valeur = input.value
+//     mealSearch(valeur)
+
+// })
 
 function searchById(idMeal) {
     fetch(id_search + idMeal)
-        .then(res => { console.log(res.json()) })
+        .then(res => { return (res.json()) })
+        .then(response => {
+            console.log(response)
+        })
 }
+url = name_search + 'Corba'
+fetch(url)
+    .then(response => console.log(response.json()))
 
+function searchByName(nameMeal) {
+    url = name_search + nameMeal
+    fetch(url)
+        .then(res => { return (res.json()) })
+        .then(data => {
+            console.log(data)
+        })
+}
+searchById(52977)
+searchByName(wrapped)
 generate_recipe.addEventListener('click', () => {
     fetch(random_search)
         .then(response => { return (response.json()) })
@@ -42,7 +62,7 @@ generate_recipe.addEventListener('click', () => {
                 img_recette.src = image
                 pop_up_title.innerHTML = meal.strMeal
                 recette.innerHTML = meal.strInstructions
-                console.log(meal.strIngredient1)
+                    // console.log(meal.strIngredient1)
             })
         })
 })
@@ -51,5 +71,13 @@ display_recipe.addEventListener('click', (e) => {
     e.preventDefault
     pop_up.style.display = 'block'
     exit.addEventListener('click', () => pop_up.style.display = 'none')
-        // alert('recette')
 })
+
+function mealSearch(valeur) {
+    if (fetch(id_search + valeur)) {
+        searchById(valeur)
+    }
+    if (fetch(name_search + valeur)) {
+        searchByName(valeur)
+    }
+}
